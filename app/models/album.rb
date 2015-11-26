655f2d9040ae923af581d66aed43fe114c04b039
d <watanabe1205@gmail.com>
2015-11-26 23:23:37 +0900

    albumモデルのテスト追加とバリデーション追加

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,2 +1,6 @@
 class Album < ActiveRecord::Base
+  validates :title, presence: true
+  validates :album_url, presence: true
+  validates :cover_url, presence: true
+  validates :count, presence: true
 end
--- spec/models/album_spec.rb
+++ spec/models/album_spec.rb
@@ -1,5 +1,41 @@
 require 'rails_helper'
 
 RSpec.describe Album, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe "validation" do
+
+    describe "title" do
+      let(:no_title_album) { Album.new(title: nil, album_url: "album_url", cover_url: "cover_url", count: 0) }
+      it "require title" do
+        expect(no_title_album).to_not be_valid
+        expect(no_title_album.errors[:title]).to_not be_empty
+      end
+    end
+
+    describe "album_url" do
+      let(:no_album_url_album) { Album.new(title: "title", album_url: nil, cover_url: "cover_url", count: 0) }
+      it "require album_url" do
+        expect(no_album_url_album).to_not be_valid
+        expect(no_album_url_album.errors[:album_url]).to_not be_empty
+      end
+    end
+
+    describe "cover_url" do
+      let(:no_cover_url_album) { Album.new(title: "title", album_url: "album_url", cover_url: nil, count: 0) }
+      it "require cover_url" do
+        expect(no_cover_url_album).to_not be_valid
+        expect(no_cover_url_album.errors[:cover_url]).to_not be_empty
+      end
+    end
+
+    describe "count" do
+      let(:no_count_album) { Album.new(title: "title", album_url: "album_url", cover_url: "cover_url", count: nil) }
+      it "require count" do
+        expect(no_count_album).to_not be_valid
+        expect(no_count_album.errors[:count]).to_not be_empty
+      end
+    end
+
+  end
+
 end
