434e3a68a08b4a75017fe040deeef62742fafaa9
d <watanabe1205@gmail.com>
2015-11-30 23:31:35 +0900

    historyのcreateアクション

--- app/controllers/histories_controller.rb
+++ app/controllers/histories_controller.rb
@@ -2,4 +2,13 @@ class HistoriesController < ApplicationController
   def index
     @histories = History.all
   end
+
+  def new
+    @history = History.new
+  end
+
+  def create
+    History.create(params.require(:history).permit(:update_date, :count, :album_id))
+    redirect_to action: :index
+  end
 end
--- spec/controllers/histories_controller_spec.rb
+++ spec/controllers/histories_controller_spec.rb
@@ -7,4 +7,23 @@ RSpec.describe HistoriesController, type: :controller do
       expect(assigns(:histories)).to_not be_nil
     end
   end
+
+  describe "#new" do
+    it "assigns @history" do
+      get :new
+      expect(assigns(:history)).to_not be_nil
+    end
+  end
+
+  describe "#create" do
+    let(:album) { Album.create(title: "title", album_url: "album_url", cover_url: "cover_url", count: 0) }
+
+    it "create new history" do
+      expect {
+        post :create, history: { update_date: Date.today, count: 0, album_id: album.id }
+      }.to change {
+        History.count
+      }.from(0).to(1)
+    end
+  end
 end
